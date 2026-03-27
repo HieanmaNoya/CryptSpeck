bd7587f4166cae13f7040c80309c71ddb9b2fec7
Дмитрий Береснев <gr943_bmnni@lab-403-2.tomtit-tomsk.ru>
2026-03-27 10:01:26 +0700

    1

--- Speck/round.php
+++ Speck/round.php
@@ -1,10 +1,10 @@
 <?php
 include __DIR__ . "/moveBytes.php";
-const mR = 3;
+const mR = 8;
 const mL = 3;
-$key = [0x3061706577896c69, 0x4012346f6e657273];
-$pt1 = 0x7061706572636c67;
-$pt2 = 0x7073646f6e657252;
+$key = [123456789010, 12345678910];
+$pt1 = 0x7061706572636c6;
+$pt2 = 0x7073646f6e65725;
 //var_dump($key[0]);
 //var_dump($key[1]);
 //var_dump($pt1);
@@ -13,17 +13,15 @@ $pt2 = 0x7073646f6e657252;
 function rounds($pt1, $pt2, $key)
 {
     $mover = new moveBytes();
-    $pt1 = gmp_init($pt1);
-    $pt2 = gmp_init($pt2);
-    $key = gmp_init($key);
+    var_dump($pt1);
     $pt1 = $mover->rotateRight($pt1, 8);
-    $pt1 = gmp_init($pt2);
-    $pt1 = ($pt1 ^ $key);
-    var_dump($pt2);
+    var_dump($pt1);
+    $pt1 = $pt1 & $pt2;
+    var_dump($pt1);
+    $pt1 ^= $key;
+    var_dump($pt1);
     $pt2 = $mover->rotateLeft($pt2, 3);
-    var_dump($pt2);
-    $pt2 = ($pt2 ^ $pt1);
-    var_dump($pt2);
+    $pt2 ^= $pt1;
     return $pt1;
 }
 
